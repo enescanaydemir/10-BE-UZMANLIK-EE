9fb5ac9fc95acd71f66eef3f5a9cf72a2d3f1f46
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2025-01-11 14:39:44 +0300

    DTOs created

--- 03-API/Week06/11-01-2025/EShop/EShop.Shared/Dtos/CartDto.cs
+++ 03-API/Week06/11-01-2025/EShop/EShop.Shared/Dtos/CartDto.cs
@@ -10,8 +10,35 @@ public class CartDto
     public bool IsActive { get; set; }
     public bool IsDeleted { get; set; }
     public string? ApplicationUserId { get; set; }
-    //public ApplicationUserDto ApplicationUser { get; set; }
-    // public ICollection<CartItemDto> CartItems { get; set; } = new List<CartItemDto>();
-    
-
+    public ApplicationUserDto ApplicationUser { get; set; } = new ApplicationUserDto();
+    public ICollection<CartItemDto> CartItems { get; set; } = new List<CartItemDto>();
+    public decimal TotalAmount => CartItems.Sum(x => x.Product.Price * x.Quantity); //cartItems içindeki her bir elemanın fiyatı ile adedini çarparak toplam fiyatı döndür.
+    public int TotalItems => CartItems == null ? 0 : CartItems.Count(); //cartItems boş ise 0 döndür, dolu ise count() ile sayısını döndür.
 }
+
+
+
+
+// public decimal TotalPrice => CartItems.Sum(x => x.Product.Price * x.Quantity);
+
+//yukarıdakinin uzun hali;
+// public decimal TotalPrice
+// {
+//     get
+//     {
+//         return CartItems.Sum(x => x.Product.Price * x.Quantity);
+//     }
+// }
+
+// public decimal TotalPrice() => CartItems.Sum(x => x.Product.Price * x.Quantity);
+
+//yukarıdakinin uzun hali;
+// public decimal TotalPrice()
+// {
+//     decimal totalPrice = 0;
+//     foreach (var item in CartItems)
+//     {
+//         totalPrice += item.Product.Price * item.Quantity;
+//     }
+//     return totalPrice;
+// }
